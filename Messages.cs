104108ded3ef2f634c0b54f107421b18d2008709
apharitonov <apharitonov@yandex.ru>
2012-11-22 17:32:49 +0400

    1

---
+++ Messages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Messaging;
+using System.Text;
+
+namespace ws_srvc_mq
+{
+    enum MessageType { mtTest = 0, mtUserMsg = 1}
+
+    public class MessageBase
+    {
+
+    }
+
+    public class Message : MessageBase
+    {
+        private MessageType _MessageType;
+        public MessageType MessageType
+        {
+            get
+            {
+                return _MessageType;
+            }
+        }
+        public Message(MessageType MessageType)
+        {
+            _MessageType = MessageType;
+        }
+    }
+
+    public class MessageService
+    { 
+        public bool RecvMsg(MessageBase msg)
+        {
+            bool ret = false;
+            using (MessageQueue queue = MessageQueue.Create(@". \myqueue"))
+            {
+                queue.Label = "First Queue";
+                
+            }
+            return ret;
+        }
+        
+    }
+
+}
